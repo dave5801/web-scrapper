aa8ac91b72b45036ab872e2d8ba9b6072ca3c7f1
David Franklin <dave5801@gmail.com>
2018-01-31 13:35:11 -0800

    writing a function and exporting

--- web_crawler.js
+++ web_crawler.js
@@ -1,1 +1,17 @@
 let searchInput = new AmazonAutocomplete('#search-input');
+
+function get_search_results(searchInput){
+    let search_results = searchInput._words;
+
+    for(var i =0; i < search_results; i++){
+        console.log(search_results[i])
+    }
+
+}
+
+export {get_search_results};
+
+
+console.log(searchInput._words);
+
+let search_results = function(searchInput._words){};
